503d20dd3a7770df5abda94b887104956251aa52
Owen Butler <179979+owenbutler@users.noreply.github.com>
2024-01-25 12:13:01 +1100

    Allow usage of procedural palettes

--- app/particles.rb
+++ app/particles.rb
@@ -5,9 +5,46 @@ def particles_tick(emitters, particles, args)
   render_particles(particles, args)
 end
 
+$palettes = [
+  [[0.5,0.5,0.5],[0.5,0.5,0.5],[1.0,1.0,1.0],[0,0.3333,0.6666]],
+  [[0.5,0.5,0.5],[0.666,0.666,0.666],[1.0,1.0,1.0],[0,0.3333,0.6666]],
+  [[0.5,0.5,0.5],[0.75,0.75,0.75],[1.0,1.0,1.0],[0,0.3333,0.6666]],
+  [[0.5,0.5,0.5],[1,1,1],[1.0,1.0,1.0],[0,0.3333,0.6666]],
+  [[1,0.5,0.5],[0.5,0.5,0.5],[0.75,1.0,0.6666],[0.8,1.0,0.3333]],
+  [[0.5,0.5,0.5],[0.5,0.5,0.5],[0.8,0.8,0.5],[0,0.2,0.5]],
+  [[0.6666,0.5,0.5],[0.5,0.6666,0.5],[0.6666,0.666,0.5],[0.2,0.0,0.5]],
+  [[0.5,0.5,0],[0.5,0.5,0],[0.5,0.5,0],[0.5,0.0,0]],
+  [[0.0,0.5,0.5],[0,0.5,0.5],[0.0,0.3333,0.5],[0.0,0.6666,0.5]],
+  [[0.5,0.5,0],[0.5,0.5,0],[0.1,0.5,0],[0.0,0.0,0]],
+  [[0.0,0.5,0.5],[0,0.5,0.5],[0.0,0.5,0.3333],[0.0,0.5,0.6666]],
+  [[0.5,0,0.5],[0.5,0,0.5],[0.5,0,0.5],[0,0,0.5]],
+  [[0.650,0.5,0.310],[-0.650,0.5,0.6],[0.333,0.278,0.278],[0.660,0.0,0.667]],
+  [[0.660,0.56,0.680],[0.718,0.438,0.720],[0.520,0.8,0.520],[-0.430,-0.397,-0.083]],
+  [[0.610,0.498,0.650],[0.388,0.498,0.350],[0.530,0.498,0.620],[3.438,3.012,4.025]],
+  [[0.731,1.098,0.192],[0.358,1.090,0.657],[1.077,0.360,0.328],[0.965,2.265,0.837]],
+  [[0.892,0.725,0.000],[0.878,0.278,0.725],[0.332,0.518,0.545],[2.440,5.043,0.732]],
+  [[0.821,0.328,0.242],[0.659,0.481,0.896],[0.612,0.340,0.296],[2.820,3.026,-0.273]],
+  [[0.938,0.328,0.718],[0.659,0.438,0.328],[0.388,0.388,0.296],[2.538,2.478,0.168]],
+  [[0.590,0.811,0.120],[0.410,0.392,0.590],[0.940,0.548,0.278],[-4.242,-6.611,-4.045]],
+]
+
+# Ruby implementation (without vectors) of the cosine based palette generator from here:
+#  https://iquilezles.org/articles/palettes/
+# Example palettes (and create your own) here:
+#  http://dev.thi.ng/gradients/
+def colors t, p_index
+  p = $palettes[p_index]
+  {
+    r: (p[0][0] + p[1][0] * (Math.cos(6.28318*(p[2][0]+t+p[3][0])))) * 255,
+    g: (p[0][1] + p[1][1] * (Math.cos(6.28318*(p[2][1]+t+p[3][1])))) * 255,
+    b: (p[0][2] + p[1][2] * (Math.cos(6.28318*(p[2][2]+t+p[3][2])))) * 255,
+  }
+end
+
 def process_emitters(emitters, particles, args)
 
   tc = args.tick_count
+  color_time = tc * 2000
 
   num_emitters = emitters.length
   index = 0
@@ -18,7 +55,7 @@ def process_emitters(emitters, particles, args)
     if next_spawn.elapsed?
 
       emitter.num.times do
-        particles << emitter.copy().merge!( {
+        particle = emitter.copy().merge!( {
           anchor_x: 0.5, anchor_y: 0.5,
           xvel: rand().remap(0, 1, emitter.xv_min, emitter.xv_max),
           yvel: rand().remap(0, 1, emitter.yv_min, emitter.yv_max),
@@ -26,6 +63,11 @@ def process_emitters(emitters, particles, args)
           angle: emitter.angle == -1 ? rand(360) : emitter.angle,
           spawn_time: tc,
         })
+        if emitter.palette
+          colors_hash = colors(color_time, emitter.palette)
+          particle.merge!(colors_hash)
+        end
+        particles << particle
       end
       emitter[:next_spawn] = args.tick_count + emitter[:freq]
     end
